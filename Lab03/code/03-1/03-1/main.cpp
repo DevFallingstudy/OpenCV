21ad55616fd6ea5408e8163f95f36412c1610fbd
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-03-29 17:05:04 +0900

    Add Lab03-1 histogram show function

--- Lab03/code/03-1/03-1/main.cpp
+++ Lab03/code/03-1/03-1/main.cpp
@@ -20,6 +20,7 @@ using namespace cv;
 using namespace std;
 
 void equalizeHistFunc(string imgName);
+Mat getHistogramImage(Mat* img);
 void smoothingFunc(string imgName);
 
 int main(int argc, const char * argv[]) {
@@ -46,6 +47,7 @@ void equalizeHistFunc(string imgName) {
     Mat histImage1(hist_h, hist_w, CV_8UC3, Scalar(0,0,0));
     Mat histImage2(hist_h, hist_w, CV_8UC3, Scalar(0,0,0));
     Mat b_hist1, b_hist2;
+    Mat histImg1, histImg2;
     
     // Show images
     img = imread(imageName.c_str(), IMREAD_COLOR);
@@ -55,31 +57,36 @@ void equalizeHistFunc(string imgName) {
     
     imshow("Original Gray", img_gray);
     imshow("Result", img_result);
-//    waitKey(0);
     
-    
-    // Show Histogram - Original
-    calcHist( &img_gray, 1, 0, Mat(), b_hist1, 1, &histSize, &histRange, uniform, accumulate );
-    normalize(b_hist1, b_hist1, 0, histImage1.rows, NORM_MINMAX, -1, Mat());
-    
-    for (int i = 1; i < histSize; i++) {
-        line( histImage1, Point( bin_w*(i-1), hist_h - cvRound(b_hist1.at<float>(i-1)) ) , Point( bin_w*(i), hist_h - cvRound(b_hist1.at<float>(i)) ), Scalar( 255, 0, 0), 2, 8, 0 );
-    }
-    
-    // Show Histogram - Result
-    calcHist( &img_result, 1, 0, Mat(), b_hist2, 1, &histSize, &histRange, uniform, accumulate );
-    normalize(b_hist2, b_hist2, 0, histImage2.rows, NORM_MINMAX, -1, Mat());
-    
-    for (int i = 1; i < histSize; i++) {
-        line( histImage2, Point( bin_w*(i-1), hist_h - cvRound(b_hist2.at<float>(i-1)) ) , Point( bin_w*(i), hist_h - cvRound(b_hist2.at<float>(i)) ), Scalar( 255, 0, 0), 2, 8, 0 );
-    }
+    histImg1 = getHistogramImage(&img_gray);
+    histImg2 = getHistogramImage(&img_result);
     
     namedWindow("Histogram Result", WINDOW_AUTOSIZE);
     namedWindow("Histogram Original", WINDOW_AUTOSIZE);
-    imshow("Histogram Result", histImage2);
-    imshow("Histogram Original", histImage1);
+    imshow("Histogram Result", histImg1);
+    imshow("Histogram Original", histImg2);
     waitKey(0);
     
+}
+
+Mat getHistogramImage(Mat* img) {
+    int histSize = 256;
+    float range[] = { 0, 256 } ;
+    const float* histRange = { range };
+    bool uniform = true, accumulate = false;
+    int hist_w = 512, hist_h = 400;
+    int bin_w = cvRound((double) hist_w / histSize);
+    Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0,0,0));
+    Mat b_hist;
+    
+    calcHist( img, 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+    
+    for (int i = 1; i < histSize; i++) {
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) , Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ), Scalar( 255, 0, 0), 2, 8, 0 );
+    }
+    
+    return histImage;
 }
 
 void smoothingFunc(string imgName) {
