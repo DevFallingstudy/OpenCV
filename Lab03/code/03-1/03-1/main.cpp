05c7e81e2c396f2e20c7a85eaac843cf2b966d86
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-03-29 16:48:13 +0900

    Add gitignore

---
+++ Lab03/code/03-1/03-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  03-1
+//
+//  Created by Fallingstar on 2018. 3. 29..
+//  Copyright © 2018년 Fallingstar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
